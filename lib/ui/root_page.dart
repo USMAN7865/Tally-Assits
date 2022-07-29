085a8c82cfded58dfcc4bd02e24ce1ac5319e368
M.Usman <shani78ok@gmail.com>
2022-07-29 17:49:42 +0500

    deoendencies bug fixed

---
+++ lib/ui/root_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+// import 'package:tassist/ui/widgets/welcomescreen.dart';
+import 'package:tassist/ui/widgets/welcomescreen2.dart';
+import 'package:tassist/ui/views/homescreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:tassist/ui/views/menu.dart';
+
+class RootPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var user;
+    // final user = Provider.of(context);
+    print(user);
+
+    // return either the Home or Menu widget
+    if (user == null) {
+      return HomeScreen();
+    } else {
+      return WelcomeScreen2();
+    }
+  }
+}
